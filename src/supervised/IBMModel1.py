5daa80dcf89f714ae5f2f62b386864c96648fe1f
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2015-06-25 16:53:45 -0700

    Added supervised ibm model 1 using the partial alignments

--- src/supervised/IBMModel1.py
+++ src/supervised/IBMModel1.py
@@ -1,4 +1,45 @@
-#!/usr/bin/env python
+from __future__ import division
+
 # -*- coding: utf-8 -*-
 
 __author__ = 'Jasneet Sabharwal <jsabharw@sfu.ca>'
+
+from collections import defaultdict
+
+
+def supervisedIBMModel1(stCoOccurrenceCount, bitext, partialAlignments):
+    """
+    Supervised version of IBM Model 1. In this the translation probability is only counted for
+    the partial annotations of alignments that we have
+    :param stCoOccurrenceCount: Dictionary containing the source and target words co-occurrence count
+    :param bitext: list of tuples containing the source and target sentences
+    :param partialAlignments: partial alignments from the partial annotations (source: [list of aligned target words])
+    :return: translation probability, t(f|e)
+    """
+
+    # translation probability
+    tProb = defaultdict(int)
+
+    stCounts = defaultdict(int)
+    tCounts = defaultdict(int)
+
+    # Collect counts
+    for (source, target) in bitext:
+        for sWord in source:
+            if sWord in partialAlignments:
+                for tWord in target:
+                    updateValue = 0
+                    if tWord in partialAlignments[sWord]:
+                        updateValue = 1
+                    stCounts[(sWord, tWord)] += updateValue
+                    tCounts[tWord] += updateValue
+
+    # Calculate probability
+    for (sWord, tWord) in stCoOccurrenceCount:
+        try:
+            tProb[(sWord, tWord)] = stCounts[(sWord, tWord)] / tCounts[tWord]
+        except ZeroDivisionError:
+            pass
+
+    return tProb
+
