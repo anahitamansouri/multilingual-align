7ab1ce81e7dcbe70fce227ea83d4e7517075f8ea
Jasneet Sabharwal <jasneet.sabharwal@gmail.com>
2015-07-14 07:22:31 -0700

    made ibm model 1 modular and added parallelized version

--- src/models/IBMModel1.py
+++ src/models/IBMModel1.py
@@ -6,6 +6,8 @@ __author__ = 'Jasneet Sabharwal <jsabharw@sfu.ca>'
 from collections import defaultdict
 from ibmModelCommons import maximizationTProb, maximizationQProb, initializeQProbUniformly, initializeTProbUniformly, \
     initializeCounts, maximizationInterpolatedTProb, maximizationInterpolatedQProb
+import multiprocessing
+import math
 
 
 def supervisedIBMModel1(stCoOccurrenceCount, bitext, partialAlignments):
@@ -36,13 +38,13 @@ def supervisedIBMModel1(stCoOccurrenceCount, bitext, partialAlignments):
                 for tIdx, tWord in enumerate(target):
                     updateValue = 0.0
                     if tWord in partialAlignments[sWord]:
-                        iValues.add(tIdx)
+                        jValues.add(tIdx)
                         updateValue = 1.0
                     stCounts[(sWord, tWord)] += updateValue
                     tCounts[tWord] += updateValue
-                    jiCounts[(sIdx, tIdx, len(target), len(source))] += updateValue
-                    iCounts[(tIdx, len(target), len(source))] += updateValue,
-                jValues.add(sIdx)
+                    jiCounts[(tIdx, sIdx, len(target), len(source))] += updateValue
+                    iCounts[(sIdx, len(target), len(source))] += updateValue
+                iValues.add(sIdx)
                 lValues.add(len(target))
                 mValues.add(len(source))
 
@@ -53,7 +55,7 @@ def supervisedIBMModel1(stCoOccurrenceCount, bitext, partialAlignments):
     return tProb, qProb
 
 
-def interpolatedIBMModel1(stCoOccurrenceCount, bitext, supervisedTProb, lWeight):
+def interpolatedIBMModel1(sourceCounts, stCoOccurrenceCount, bitext, supervisedTProb, lWeight):
     """
     Interpolated version of IBM Model1. It is the standard unsupervised IBM Model 1 but linearly interpolates
     with supervised translation probabilities during maximization step
@@ -68,30 +70,13 @@ def interpolatedIBMModel1(stCoOccurrenceCount, bitext, supervisedTProb, lWeight)
     """
 
     # Initialize tProb uniformly
-    tProb = initializeTProbUniformly(stCoOccurrenceCount)
+    tProb = initializeTProbUniformly(sourceCounts, stCoOccurrenceCount)
     qProb, jValues, iValues, lValues, mValues = initializeQProbUniformly(bitext)
 
     for emIter in range(10):
-        stCounts, tCounts, jiCounts, iCounts = initializeCounts()
 
         # Collect counts
-        for (source, target) in bitext:
-            lValues.add(len(target))
-            mValues.add(len(source))
-            for sIdx, sWord in enumerate(source):
-                normalizer = 0
-                jValues.add(sIdx)
-
-                for tWord in target:
-                    normalizer += tProb[(sWord, tWord)]
-
-                for tIdx, tWord in enumerate(target):
-                    iValues.add(tIdx)
-                    updateValue = tProb[(sWord, tWord)]/normalizer
-                    stCounts[(sWord, tWord)] += updateValue
-                    tCounts[tWord] += updateValue
-                    jiCounts[(sIdx, tIdx, len(target), len(source))] += updateValue
-                    iCounts[(tIdx, len(target), len(source))] += updateValue
+        stCounts, tCounts, jiCounts, iCounts = _expectation(bitext, tProb)
 
         # Calculate and interpolate probabilities
         tProb = maximizationInterpolatedTProb(stCoOccurrenceCount, stCounts, tCounts, tProb, supervisedTProb, lWeight)
@@ -100,7 +85,7 @@ def interpolatedIBMModel1(stCoOccurrenceCount, bitext, supervisedTProb, lWeight)
     return tProb, qProb
 
 
-def unsupervisedIBMModel1(stCoOccurrenceCount, bitext):
+def unsupervisedIBMModel1(sourceCounts, stCoOccurrenceCount, bitext):
     """
     Unsupervised version of IBM Model 1. In this the translation probability is only counted for
     the partial annotations of alignments that we have
@@ -112,7 +97,77 @@ def unsupervisedIBMModel1(stCoOccurrenceCount, bitext):
     :return: translation probability, t(f|e)
     """
     # Initialize translation probability uniformly
-    tProb = initializeTProbUniformly(stCoOccurrenceCount)
+    tProb = initializeTProbUniformly(sourceCounts, stCoOccurrenceCount)
+
+    # Initialize alignment probability uniformly
+    qProb, jValues, iValues, lValues, mValues = initializeQProbUniformly(bitext)
+
+    for emIter in range(10):
+
+        # Calculate Counts
+        stCounts, tCounts, jiCounts, iCounts = _expectation(bitext, tProb)
+
+        # Calculate probability
+        tProb = maximizationTProb(stCoOccurrenceCount, stCounts, tCounts, tProb)
+        qProb = maximizationQProb(qProb, jiCounts, iCounts, jValues, iValues, lValues, mValues)
+
+    return tProb, qProb
+
+
+def parallelInterpolatedIBMModel1(sourceCounts, stCoOccurrenceCount, bitext, supervisedTProb, lWeight):
+    """
+    Interpolated version of IBM Model1. It is the standard unsupervised IBM Model 1 but linearly interpolates
+    with supervised translation probabilities during maximization step
+
+    :param sVocabCount: source vocab occurrence count
+    :param tVocabCount: target vocab occurrence count
+    :param stCoOccurrenceCount: source and target word co-occurrence count
+    :param bitext: parallel source and target sentences
+    :param supervisedTProb: translation probability, t(f|e), from supervised IBM Model 1
+    :param lWeight: lambda for linear interpolation of two models
+    :return: translation probability, t(f|e)
+    """
+
+    # Initialize tProb uniformly
+    tProb = initializeTProbUniformly(sourceCounts, stCoOccurrenceCount)
+    qProb, jValues, iValues, lValues, mValues = initializeQProbUniformly(bitext)
+
+    for emIter in range(10):
+        stCounts, tCounts, jiCounts, iCounts = initializeCounts()
+
+        # Collect counts
+        outputQueue = multiprocessing.Queue()
+        numberOfProcessAllowed = multiprocessing.cpu_count()
+        chunkSize = int(math.ceil(len(bitext) / float(numberOfProcessAllowed)))
+        procs = []
+
+        for procNum in range(numberOfProcessAllowed):
+            bitextChunk = bitext[chunkSize*procNum:chunkSize*(procNum+1)]
+            proc = multiprocessing.Process(target=_parallelExpectation, args=(bitextChunk, tProb,
+                                                                              outputQueue))
+            procs.append(proc)
+            proc.start()
+
+        for procNum in range(numberOfProcessAllowed):
+            partialResult = outputQueue.get()
+            stCounts = _mergeCounts(partialResult['stCounts'], stCounts)
+            tCounts = _mergeCounts(partialResult['tCounts'], tCounts)
+            jiCounts = _mergeCounts(partialResult['jiCounts'], jiCounts)
+            iCounts = _mergeCounts(partialResult['iCounts'], iCounts)
+
+        for proc in procs:
+            proc.join()
+
+        # Calculate and interpolate probabilities
+        tProb = maximizationInterpolatedTProb(stCoOccurrenceCount, stCounts, tCounts, tProb, supervisedTProb, lWeight)
+        qProb = maximizationQProb(qProb, jiCounts, iCounts, jValues, iValues, lValues, mValues)
+
+    return tProb, qProb
+
+
+def parallelUnsupervisedIBMModel1(sourceCounts, stCoOccurrenceCount, bitext):
+    # Initialize translation probability uniformly
+    tProb = initializeTProbUniformly(sourceCounts, stCoOccurrenceCount)
 
     # Initialize alignment probability uniformly
     qProb, jValues, iValues, lValues, mValues = initializeQProbUniformly(bitext)
@@ -121,26 +176,61 @@ def unsupervisedIBMModel1(stCoOccurrenceCount, bitext):
         stCounts, tCounts, jiCounts, iCounts = initializeCounts()
 
         # Collect counts
-        for (source, target) in bitext:
-            lValues.add(len(target))
-            mValues.add(len(source))
-            for sIdx, sWord in enumerate(source):
-                normalizer = 0.0
-                jValues.add(sIdx)
+        outputQueue = multiprocessing.Queue()
+        numberOfProcessAllowed = multiprocessing.cpu_count()
+        chunkSize = int(math.ceil(len(bitext) / float(numberOfProcessAllowed)))
+        procs = []
 
-                for tWord in target:
-                    normalizer += tProb[(sWord, tWord)]
+        for procNum in range(numberOfProcessAllowed):
+            bitextChunk = bitext[chunkSize*procNum:chunkSize*(procNum+1)]
+            proc = multiprocessing.Process(target=_parallelExpectation, args=(bitextChunk, tProb, outputQueue))
+            procs.append(proc)
+            proc.start()
 
-                for tIdx, tWord in enumerate(target):
-                    iValues.add(tIdx)
-                    updateValue = tProb[(sWord, tWord)]/normalizer
-                    stCounts[(sWord, tWord)] += updateValue
-                    tCounts[tWord] += updateValue
-                    jiCounts[(sIdx, tIdx, len(target), len(source))] += updateValue
-                    iCounts[(tIdx, len(target), len(source))] += updateValue
+        for procNum in range(numberOfProcessAllowed):
+            partialResult = outputQueue.get()
+            stCounts = _mergeCounts(partialResult['stCounts'], stCounts)
+            tCounts = _mergeCounts(partialResult['tCounts'], tCounts)
+            jiCounts = _mergeCounts(partialResult['jiCounts'], jiCounts)
+            iCounts = _mergeCounts(partialResult['iCounts'], iCounts)
+
+        for proc in procs:
+            proc.join()
 
         # Calculate probability
         tProb = maximizationTProb(stCoOccurrenceCount, stCounts, tCounts, tProb)
         qProb = maximizationQProb(qProb, jiCounts, iCounts, jValues, iValues, lValues, mValues)
 
     return tProb, qProb
+
+
+def _expectation(bitext, tProb):
+    stCounts, tCounts, jiCounts, iCounts = initializeCounts()
+
+    # Collect counts
+    for (source, target) in bitext:
+        for sIdx, sWord in enumerate(source):
+            normalizer = 0.0
+
+            for tWord in target:
+                normalizer += tProb[(sWord, tWord)]
+
+            for tIdx, tWord in enumerate(target):
+                updateValue = tProb[(sWord, tWord)]/normalizer
+                stCounts[(sWord, tWord)] += updateValue
+                tCounts[tWord] += updateValue
+                jiCounts[(tIdx, sIdx, len(target), len(source))] += updateValue
+                iCounts[(sIdx, len(target), len(source))] += updateValue
+
+    return stCounts, tCounts, jiCounts, iCounts
+
+
+def _parallelExpectation(bitext, tProb, outputQueue):
+    stCounts, tCounts, jiCounts, iCounts = _expectation(bitext, tProb)
+    outputQueue.put({'stCounts': stCounts, 'tCounts': tCounts, 'jiCounts': jiCounts, 'iCounts': iCounts})
+
+
+def _mergeCounts(partialCountDict, countDict):
+    for key, value in partialCountDict.iteritems():
+        countDict[key] += value
+    return countDict
